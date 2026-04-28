b4ff15edc715c5b2dc36d99738c83397113f1af0
vadym-bondar-g <evansfalter@gmail.com>
2026-04-28 16:51:57 +0200

    Created style but couldn't load from the file but from the class everything is working

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,57 @@
 // Created by user on 4/28/26.
 //
 #include <QApplication>
-#include <QMessageBox>
+#include<QLabel>
 #include<QWidget>
+#include <QFile>
+#include <QString>
 
 #include "main.h"
 
 
+
+void loadStyle(QApplication &app,const QString &stylePath) {
+    QFile file(stylePath);
+    if (!file.open(QFile::ReadOnly | QFile::Text)) {
+        QString style = file.readAll();
+        app.setStyleSheet(style);
+    }
+};
+void checkLoadStyle() {
+    QFile file(":/style/style.qss");
+    qDebug()<<"exists:"<<file.exists();
+    if (file.open(QFile::ReadOnly | QFile::Text)) {
+        qDebug()<<"opened";
+    }
+    else {
+        qDebug()<<"open failed";
+    }
+}
 int main(int argc, char *argv[]){
 QApplication app(argc, argv);
+ // loadStyle(app,":/styles/styles.qss");checkLoadStyle();
 QLabel label("Yet another Task Manager");
 label.show();
+
+    app.setStyleSheet(R"(
+QMainWindow {
+    background-color: #0b0f14;
+}
+
+QFrame {
+    background-color: rgba(20, 30, 45, 180);
+    border: 1px solid rgba(0, 170, 255, 120);
+    border-radius: 14px;
+}
+
+QPushButton {
+    background-color: rgba(10, 20, 35, 200);
+    color: #cfe8ff;
+    border-radius: 10px;
+}
+)");
+    this->setWindowFlags(Qt::FramelessWindowHint);
+    this->setWindowAttribute(Qt::WA_TranslucentBackground);
 
 
 return app.exec();}
